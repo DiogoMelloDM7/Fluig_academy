cced7cf2602f632ea04a9bbd6814e6defa6494ab
Diogo Mello <136912625+DiogoMelloDM7@users.noreply.github.com>
2024-10-15 16:54:56 -0300

    Add files via upload

--- ProjetoDSPeriodo/wcm/widget/DatasetPeriodo/src/main/resources/edit.ftl
+++ ProjetoDSPeriodo/wcm/widget/DatasetPeriodo/src/main/resources/edit.ftl
@@ -1,47 +1,107 @@
 <div id="MyWidget_${instanceId}" class="super-widget wcm-widget-class fluig-style-guide" data-params="MyWidget.instance()">
-    <div class="row">
-        <form role="form">
-            <div class="form-group col-md-3">
-                <label for="periodoInicial">Selecione a data inicial</label>
-                <input type="text" name="periodoInicial" id="periodoInicial" class="form-control">
+    <div>
+        <h1>Consulta de Liberação de Lote</h1>
+    </div>
+    
+    <div class="panel panel-primary mb-5">
+        <div class="panel-heading">
+            <h3 class="panel-title">Busca por período</h3>
+        </div>
+        <div class="panel-body">
+            <div class="row">
+                <form role="form">
+                    <div class="form-group col-md-3">
+                        <label for="periodoInicial">Selecione a data inicial</label>
+                        <input type="date" name="periodoInicial" id="periodoInicial" class="form-control">
+                    </div>
+        
+                    <div class="form-group col-md-3">
+                        <label for="periodoFinal">Selecione a data Final</label>
+                        <input type="date" name="periodoFinal" id="periodoFinal" class="form-control">
+                    </div>
+        
+                    <div class="col-md-3 form-group">
+                        <label for="pesquisarPeriodo">&nbsp;</label>
+                        <button type="button" id="pesquisarPeriodo" class="btn btn-sm btn-success" data-pesquisarPeriodo>Buscar</button>
+                    </div>
+                </form>
             </div>
-
-            <div class="form-group col-md-3">
-                <label for="periodoFinal">Selecione a data Final</label>
-                <input type="text" name="periodoFinal" id="periodoFinal" class="form-control">
-            </div>
-
-            <div class="col-md-3">
-                <button type="button" id="pesquisarPeriodo" class="btn btn-sm btn-success" data-pesquisarPeriodo>Buscar</button>
-            </div>
-        </form>
+        </div>
     </div>
 
-    <div class="row" id="divTabelaPesquisa">   
-        <table id="tabelaPesquisa" class="table table-striped">
-            <thead>
+        <div class="panel panel-primary">
+            <div class="panel-heading">
+                <h3 class="panel-title">Busca por Produto</h3>
+            </div>
+            <div class="panel-body">
+                <div class="row">
+                    <form role="form">
+                        <div class="form-group col-md-3">
+                            <label for="idProduto">Digite o código do Produto</label>
+                            <input type="text" name="idProduto" id="idProduto" class="form-control">
+                        </div>
+            
+                        <div class="col-md-3 form-group">
+                            <label>&nbsp;</label>
+                            <button type="button" id="pesquisarProduto" class="btn btn-sm btn-success" data-pesquisarProduto>Buscar</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
 
-            </thead>
+        <div class="panel panel-primary">
+            <div class="panel-heading">
+                <h3 class="panel-title">Busca por Produto e Período</h3>
+            </div>
+            <div class="panel-body">
+                <div class="row">
+                    <form role="form">
+                        <div class="form-group col-md-3">
+                            <label for="idProduto">Digite o código do Produto</label>
+                            <input type="text" name="idProdutoEPeriodo" id="idProdutoEPeriodo" class="form-control">
+                        </div>
+                        <div class="form-group col-md-3">
+                            <label for="periodoInicial">Selecione a data inicial</label>
+                            <input type="date" name="periodoEProdutoInicial" id="periodoEProdutoInicial" class="form-control">
+                        </div>
+            
+                        <div class="form-group col-md-3">
+                            <label for="periodoFinal">Selecione a data Final</label>
+                            <input type="date" name="periodoEProdutoFinal" id="periodoEProdutoFinal" class="form-control">
+                        </div>
+            
+                        <div class="col-md-3 form-group">
+                            <label>&nbsp;</label>
+                            <button type="button" id="pesquisarProdutoEPeriodo" class="btn btn-sm btn-success" data-pesquisarProdutoEPeriodo>Buscar</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
 
-            <tbody>
-
-            </tbody>
-
-            <tfoot>
-
-            </tfoot>
-        </table>
-    </div>
-
-    <div id="tabelaFiltrada">
-
-    </div>
-
+        <div class="panel panel-primary">
+            <div class="panel-body">
+                <div class="row" id="divTabelaPesquisa">   
+                    <table id="tabelaPesquisa" class="table table-striped table-bordered table-hover">
+                        <thead>
+            
+                        </thead>
+            
+                        <tbody>
+            
+                        </tbody>
+            
+                        <tfoot>
+            
+                        </tfoot>
+                    </table>
+                </div>
+            </div>
+         </div>
 </div>
+    
 
-<script>
-	let mySimpleCalendar = FLUIGC.calendar('#periodoInicial');
-    let mySimpleCalendar2 = FLUIGC.calendar('#periodoFinal');
-</script>
+
 
 <script type="text/javascript" src="/webdesk/vcXMLRPC.js"></script>
--- ProjetoDSPeriodo/wcm/widget/DatasetPeriodo/src/main/webapp/resources/js/DatasetPeriodo.js
+++ ProjetoDSPeriodo/wcm/widget/DatasetPeriodo/src/main/webapp/resources/js/DatasetPeriodo.js
@@ -3,72 +3,164 @@ var MyWidget = SuperWidget.extend({
     variavelNumerica: null,
     variavelCaracter: null,
     dsLiberacaoLotes: null,
-
+    dsTableLiberacao: null,
     //método iniciado quando a widget é carregada
     init: function() {
+        this.constroiDataTable();
     },
+
+
   
     //BIND de eventos
     bindings: {
         local: {
-            'pesquisarPeriodo': ['click_showNewDatasetFull']
+            'pesquisarPeriodo': ['click_showNewDatasetPeriodo'],
+            'pesquisarProduto': ['click_showNewDatasetProduto'],
+            'pesquisarProdutoEPeriodo': ['click_showNewDatasetProdutoEPeriodo']
+
         },
         global: {}
     },
+
+    constroiDataTable : function(conteudo){
+        let ds = new DataTable("#tabelaPesquisa");
+        console.log(ds)
+    },
  
-    retornaDsLiberacaoLotes : function(){
-        let dataInicial = $("#periodoInicial").val()
-        let dataFinal = $("#periodoFinal").val()
+    retornaDsLiberacaoLotesPeriodo : function(){
+            let dataInicial = $("#periodoInicial").val()
+            let dataFinal = $("#periodoFinal").val()
+            if(dataInicial != "" && dataFinal != ""){
+                let partesDataInicial = dataInicial.split('-');
+                let partesDataFinal = dataFinal.split('-');
+        
+                dataInicial = `${partesDataInicial[0]}${partesDataInicial[1]}${partesDataInicial[2]}`;
+                dataFinal = `${partesDataFinal[0]}${partesDataFinal[1]}${partesDataFinal[2]}`;
 
-        let partesDataInicial = dataInicial.split('/');
-        let partesDataFinal = dataFinal.split('/');
-
-        dataInicial = `${partesDataInicial[2]}${partesDataInicial[1]}${partesDataInicial[0]}`;
-        dataFinal = `${partesDataFinal[2]}${partesDataFinal[1]}${partesDataFinal[0]}`;
-
-        console.log(dataInicial, dataFinal)
-
-
-        let filtroGrupo = DatasetFactory.createConstraint("DD_DTVALID", dataInicial, dataFinal, ConstraintType.MUST);
-        let dsLiberacaoLotes = DatasetFactory.getDataset("dsLiberacaoLotesProtheus", null, new Array(filtroGrupo), null);
-        return dsLiberacaoLotes
-    },
-
-    showNewDatasetFull :function () {
-        let div = document.getElementById("tabelaFiltrada");
-        div.empty();
-         
-        //Busca o dataset
-        try {
-            let dataset = this.retornaDsLiberacaoLotes();
-            div.innerHTML = this.showDataset(dataset);
-        } catch(erro) {
-            div.innerHTML = erro;
-        }
-    },
-     
-     showDataset: function(dataset) {
-        let tabela = "<table class='table table-striped'>";
-         
-        //Monta o cabeçalho
-        tabela += "<tr>";
-        for (let i = 0; i < dataset.columns.length; i++) {
-            tabela += "<th>" + dataset.columns[i] + "</th>";
-        }
-        tabela += "</tr>";
-     
-        //Monta os registros   
-        for (let x = 0; x < dataset.values.length; x++) {
-            tabela += "<tr>";
-            let row = dataset.values[x];
-            for (let y = 0; y < dataset.columns.length; y++) {
-                tabela += "<td>" + row[dataset.columns[y]] + "</td>";
+                let filtroGrupo = DatasetFactory.createConstraint("periodo", dataInicial, dataFinal, ConstraintType.MUST);
+                let dsLiberacaoLotes = DatasetFactory.getDataset("dsLiberacaoLotesProtheus", null, new Array(filtroGrupo), null);
+                return dsLiberacaoLotes
             }
-            tabela += "</tr>";
+            console.log(dataInicial, dataFinal)
+            return alert("Preencha os campos corretamente!");
+                 
+    },
+
+    retornaDsLiberacaoLotesProduto : function(){
+        let codigo = parseInt($("#idProduto").val())
+        
+        if(codigo){
+            let filtroGrupo = DatasetFactory.createConstraint("produtos", codigo, codigo, ConstraintType.MUST);
+            let dsLiberacaoLotes = DatasetFactory.getDataset("dsLiberacaoLotesProtheus", null, new Array(filtroGrupo), null);
+            return dsLiberacaoLotes
         }
-         
-        tabela += "</table>";
-        return tabela;
+        console.log(codigo)
+        return alert("Preencha o campo antes de pesquisar!");     
+    },
+
+    retornaDsLiberacaoLotesProdutoEPeriodo : function(){
+        let codigo = parseInt($("#idProdutoEPeriodo").val())
+        let dataInicial = $("#periodoEProdutoInicial").val()
+        let dataFinal = $("#periodoEProdutoFinal").val()
+        
+        if(codigo && dataInicial != "" && dataFinal != ""){
+            let partesDataInicial = dataInicial.split('-');
+            let partesDataFinal = dataFinal.split('-');
+
+            dataInicial = `${partesDataInicial[0]}${partesDataInicial[1]}${partesDataInicial[2]}`;
+            dataFinal = `${partesDataFinal[0]}${partesDataFinal[1]}${partesDataFinal[2]}`;
+
+
+            let filtro1 = DatasetFactory.createConstraint("produtos", codigo, codigo, ConstraintType.MUST);
+            let filtro2 = DatasetFactory.createConstraint("periodo", dataInicial, dataFinal, ConstraintType.MUST);
+            let constraints = new Array(filtro1, filtro2);
+            let dsLiberacaoLotes = DatasetFactory.getDataset("dsLiberacaoLotesProtheus", null, constraints, null);
+            return dsLiberacaoLotes
+        }
+        console.log(codigo)
+        return alert("Preencha os campo corretamente!");     
+    },
+
+    showNewDatasetProdutoEPeriodo: function () {
+        let $table = $("#tabelaPesquisa");
+        let $thead = $table.find("thead");
+        let $tbody = $table.find("tbody");
+
+        $thead.empty();
+        $tbody.empty();
+        $("#idProduto").val("");
+        $("#periodoInicial").val("");
+        $("#periodoFinal").val("");
+
+
+        try {
+            let dataset = this.retornaDsLiberacaoLotesProdutoEPeriodo();
+            this.showDataset(dataset);
+        } catch (erro) {
+            $tbody.append("<tr><td colspan='100%'>" + erro + "</td></tr>");
+        }
+    },
+
+    showNewDatasetPeriodo: function () {
+        let $table = $("#tabelaPesquisa");
+        let $thead = $table.find("thead");
+        let $tbody = $table.find("tbody");
+
+        $thead.empty();
+        $tbody.empty();
+        $("#idProduto").val("");
+        $('#idProdutoEPeriodo').val("");
+        $("#periodoEProdutoInicial").val("")
+        $("#periodoEProdutoFinal").val("")
+
+
+        try {
+            let dataset = this.retornaDsLiberacaoLotesPeriodo();
+            this.showDataset(dataset);
+        } catch (erro) {
+            $tbody.append("<tr><td colspan='100%'>" + erro + "</td></tr>");
+        }
+    },
+
+    showNewDatasetProduto: function () {
+        let $table = $("#tabelaPesquisa");
+        let $thead = $table.find("thead");
+        let $tbody = $table.find("tbody");
+
+        $thead.empty();
+        $tbody.empty();
+        $('#idProdutoEPeriodo').val("");
+        $("#periodoEProdutoInicial").val("");
+        $("#periodoEProdutoFinal").val("");
+        $("#periodoInicial").val("");
+        $("#periodoFinal").val("");
+
+        try {
+            let dataset = this.retornaDsLiberacaoLotesProduto();
+            this.showDataset(dataset);
+        } catch (erro) {
+            $tbody.append("<tr><td colspan='100%'>" + erro + "</td></tr>");
+        }
+    },
+    
+    showDataset: function (dataset) {
+        let $table = $("#tabelaPesquisa");
+        let $thead = $table.find("thead");
+        let $tbody = $table.find("tbody");
+
+        let $headerRow = $("<tr></tr>");
+        dataset.columns.forEach(function (column) {
+            $headerRow.append("<th>" + column + "</th>");
+        });
+        $thead.append($headerRow);
+
+        dataset.values.forEach(function (row) {
+            let $row = $("<tr></tr>");
+            dataset.columns.forEach(function (column) {
+                $row.append("<td>" + row[column] + "</td>");
+            });
+            $tbody.append($row);
+        });
     }
 
 });
